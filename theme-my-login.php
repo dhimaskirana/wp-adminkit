445b0c32133ff82c62c3f180aa9c620f4d451e6b
Dhimas Kirana <dhimaskirana@live.com>
2022-06-18 12:28:33 +0700

    fix login style

--- theme-my-login.php
+++ theme-my-login.php
@@ -5,8 +5,33 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
     <?php wp_head(); ?>
+    <style>
+        .tml-links {
+            list-style: none;
+            margin: 0;
+            padding: 0;
+        }
+
+        .tml .tml-action-confirmaction .success,
+        .tml .tml-error,
+        .tml .tml-message,
+        .tml .tml-success {
+            box-shadow: none;
+        }
+
+        .tml .tml-error {
+            background-color: #dc323210;
+        }
+
+        .tml .tml-message {
+            background-color: #00a0d210;
+        }
+
+        .tml .tml-success {
+            background-color: #46b45010;
+        }
+    </style>
 </head>
 
 <body <?php body_class(); ?>>
@@ -16,7 +41,6 @@
             <div class="row vh-100">
                 <div class="col-sm-10 col-md-8 col-lg-6 mx-auto d-table h-100">
                     <div class="d-table-cell align-middle">
-
                         <div class="card">
                             <div class="card-body">
                                 <div class="text-center mt-4">
